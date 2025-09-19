c6ad628f2d0d30053ad429d51a5edd0e51dd3dba
Nick Karpov <nick.karpov@databricks.com>
2025-09-19 21:34:40 +0000

    WIP: refactor to stages
    define stages as notebooks in ./notebooks directory; TODO: complete job definition for initialization (WIP) and convert the init notebook to simply deploy that job

---
+++ notebooks/agent.py
@@ -0,0 +1,170 @@
+from typing import Any, Generator, Optional, Sequence, Union
+
+import mlflow
+from databricks_langchain import (
+    ChatDatabricks,
+    VectorSearchRetrieverTool,
+    DatabricksFunctionClient,
+    UCFunctionToolkit,
+    set_uc_function_client,
+)
+from langchain_core.language_models import LanguageModelLike
+from langchain_core.runnables import RunnableConfig, RunnableLambda
+from langchain_core.tools import BaseTool
+from langgraph.graph import END, StateGraph
+from langgraph.graph.graph import CompiledGraph
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.prebuilt.tool_node import ToolNode
+from mlflow.langchain.chat_agent_langgraph import ChatAgentState, ChatAgentToolNode
+from mlflow.pyfunc import ChatAgent
+from mlflow.types.agent import (
+    ChatAgentChunk,
+    ChatAgentMessage,
+    ChatAgentResponse,
+    ChatContext,
+)
+
+mlflow.langchain.autolog()
+
+client = DatabricksFunctionClient()
+set_uc_function_client(client)
+
+############################################
+# Define your LLM endpoint and system prompt
+############################################
+LLM_ENDPOINT_NAME = f"databricks-meta-llama-3-3-70b-instruct"
+llm = ChatDatabricks(endpoint=LLM_ENDPOINT_NAME)
+
+system_prompt = """You are RefundGPT, a CX agent responsible for refunding late food delivery orders.
+
+    You can call tools to gather the information you need. Start with an `order_id`.
+
+    Instructions:
+    1. Call `order_details(order_id)` first to get event history and confirm the id is valid and the order was delivered.
+    2. Figure out the delivery duration by calling `get_order_delivery_time(order_id)`.
+    3. Extract the location (either directly or from the first event's body).
+    4. Call `get_location_timings(location)` to get the P50/P75/P99 values.
+    5. Compare actual delivery time to those percentiles to decide on a fair refund.
+
+    Only provide refunds for late orders, and use only the tool call results to determine whether a refund is appropriate.
+
+    Do not provide any refund for orders arriving before the P75 delivery time value.
+
+    Output a single-line JSON with these fields:
+    - `refund_usd` (float),
+    - `refund_class` ("none" | "partial" | "full"),
+    - `reason` (short human explanation of whether the order was late and, if late, how late the order was)
+
+    You must return only the JSON. No extra text or markdown."""
+
+###############################################################################
+## Define tools for your agent, enabling it to retrieve data or take actions
+## beyond text generation
+## To create and see usage examples of more tools, see
+## https://docs.databricks.com/generative-ai/agent-framework/agent-tool.html
+###############################################################################
+tools = []
+
+uc_tool_names = [f"cazzper.ai.get_order_details", 
+                 f"cazzper.ai.get_location_timings",
+                 f"cazzper.ai.get_order_delivery_time"]
+uc_toolkit = UCFunctionToolkit(function_names=uc_tool_names)
+tools.extend(uc_toolkit.tools)
+
+#####################
+## Define agent logic
+#####################
+
+
+def create_tool_calling_agent(
+    model: LanguageModelLike,
+    tools: Union[Sequence[BaseTool], ToolNode],
+    system_prompt: Optional[str] = None,
+) -> CompiledGraph:
+    model = model.bind_tools(tools)
+
+    # Define the function that determines which node to go to
+    def should_continue(state: ChatAgentState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        # If there are function calls, continue. else, end
+        if last_message.get("tool_calls"):
+            return "continue"
+        else:
+            return "end"
+
+    if system_prompt:
+        preprocessor = RunnableLambda(
+            lambda state: [{"role": "system", "content": system_prompt}]
+            + state["messages"]
+        )
+    else:
+        preprocessor = RunnableLambda(lambda state: state["messages"])
+    model_runnable = preprocessor | model
+
+    def call_model(
+        state: ChatAgentState,
+        config: RunnableConfig,
+    ):
+        response = model_runnable.invoke(state, config)
+
+        return {"messages": [response]}
+
+    workflow = StateGraph(ChatAgentState)
+
+    workflow.add_node("agent", RunnableLambda(call_model))
+    workflow.add_node("tools", ChatAgentToolNode(tools))
+
+    workflow.set_entry_point("agent")
+    workflow.add_conditional_edges(
+        "agent",
+        should_continue,
+        {
+            "continue": "tools",
+            "end": END,
+        },
+    )
+    workflow.add_edge("tools", "agent")
+
+    return workflow.compile()
+
+
+class LangGraphChatAgent(ChatAgent):
+    def __init__(self, agent: CompiledStateGraph):
+        self.agent = agent
+
+    def predict(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> ChatAgentResponse:
+        request = {"messages": self._convert_messages_to_dict(messages)}
+
+        messages = []
+        for event in self.agent.stream(request, stream_mode="updates"):
+            for node_data in event.values():
+                messages.extend(
+                    ChatAgentMessage(**msg) for msg in node_data.get("messages", [])
+                )
+        return ChatAgentResponse(messages=messages)
+
+    def predict_stream(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> Generator[ChatAgentChunk, None, None]:
+        request = {"messages": self._convert_messages_to_dict(messages)}
+        for event in self.agent.stream(request, stream_mode="updates"):
+            for node_data in event.values():
+                yield from (
+                    ChatAgentChunk(**{"delta": msg}) for msg in node_data["messages"]
+                )
+
+
+# Create the agent object, and specify it as the agent object to use when
+# loading the agent back for inference via mlflow.models.set_model()
+agent = create_tool_calling_agent(llm, tools, system_prompt)
+AGENT = LangGraphChatAgent(agent)
+mlflow.models.set_model(AGENT)
